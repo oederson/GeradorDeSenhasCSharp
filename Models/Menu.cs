b58c2d14c266b3044dc0cb2b961908e2d3aaff5c
oederson <eder.hu@live.com.pt>
2023-10-12 17:49:02 -0300

    Codigo refatorado

--- Models/Menu.cs
+++ Models/Menu.cs
@@ -1,48 +1,48 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace GeradorDeSenhasCSharp.Models
 {
     public class Menu
     {
         public static void ExibirMenu()
         {
+            Senha senha = new();
+            var entradas = new Dictionary<string, int>()
+            {
+                {"Maiúsculas",0},
+                {"Minúsculas",0},
+                {"Números",0},
+                {"Especiais",0}
+            };
+
             Console.WriteLine(@"
 █████▀████████████████████████████████████████████████████████████████████████████████████████
 █─▄▄▄▄█▄─▄▄─█▄─▄▄▀██▀▄─██▄─▄▄▀█─▄▄─█▄─▄▄▀███▄─▄▄▀█▄─▄▄─███─▄▄▄▄█▄─▄▄─█▄─▀█▄─▄█─█─██▀▄─██─▄▄▄▄█
 █─██▄─██─▄█▀██─▄─▄██─▀─███─██─█─██─██─▄─▄████─██─██─▄█▀███▄▄▄▄─██─▄█▀██─█▄▀─██─▄─██─▀─██▄▄▄▄─█
 ▀▄▄▄▄▄▀▄▄▄▄▄▀▄▄▀▄▄▀▄▄▀▄▄▀▄▄▄▄▀▀▄▄▄▄▀▄▄▀▄▄▀▀▀▄▄▄▄▀▀▄▄▄▄▄▀▀▀▄▄▄▄▄▀▄▄▄▄▄▀▄▄▄▀▀▄▄▀▄▀▄▀▄▄▀▄▄▀▄▄▄▄▄▀");
+            Console.WriteLine("");
             Console.WriteLine("Bem-vindo ao Gerador de Senhas!");
-
-            var caracteresPossiveis = new Dictionary<string, string>();
-            caracteresPossiveis.Add("Maiusculas", "ABCDEFGHIJLMNOPQRSTUVXZ");
-            caracteresPossiveis.Add("Minusculas", "abcdefghijlmnopqrstuvxz");
-            caracteresPossiveis.Add("Numeros", "1234567890");
-            caracteresPossiveis.Add("Especiais", "%$#@!");
-
             Console.WriteLine("");
-            Console.WriteLine("Digite a quantidade de cada tipo de caractere desejado:");
-            Console.Write("Maiúsculas: ");
-            int maiusculas = int.Parse(Console.ReadLine());
-
-            Console.Write("Minúsculas: ");
-            int minusculas = int.Parse(Console.ReadLine());
-
-            Console.Write("Números: ");
-            int numeros = int.Parse(Console.ReadLine());
-
-            Console.Write("Caracteres Especiais: ");
-            int especiais = int.Parse(Console.ReadLine());
-
-            var senha = new Senha(caracteresPossiveis);
-            string senhaGerada = senha.GerarSenha(maiusculas, minusculas, numeros, especiais, caracteresPossiveis);
+            Console.WriteLine("Para cada tipo de caracter digite a quantidade que deseja");
             Console.WriteLine("");
-            Console.WriteLine($"Sua senha gerada com {maiusculas} maiusculas,{minusculas} minusculas, {numeros} numeros, {especiais} especiais é : {senhaGerada} ");
-        }
-        
+            foreach(var entrada in entradas)
+            {
+                Console.Write($"{entrada.Key} : ");
+                string oQueDigitou = Console.ReadLine();
+                if (int.TryParse(oQueDigitou, out int quantidade))
+                {
+                    entradas[entrada.Key] = quantidade;
+                }
+                else
+                {
+                    Console.WriteLine("Voce não digitou um número valido a quantidade será 0");
+                }
+            }
 
-        
+            string senhaGerada = senha.GerarSenha(entradas["Maiúsculas"], 
+                                                   entradas["Minúsculas"], 
+                                                   entradas["Números"], 
+                                                   entradas["Especiais"]);
+             Console.WriteLine("");
+             Console.WriteLine($"Sua senha gerada com {entradas["Maiúsculas"]} Maiúsculas , {entradas["Minúsculas"]} Minúsculas, {entradas["Números"]} Números, {entradas["Especiais"]} Especiais é : {senhaGerada} ");
+        }           
     }
 }
--- Models/Senha.cs
+++ Models/Senha.cs
@@ -4,39 +4,35 @@ namespace GeradorDeSenhasCSharp.Models
 {
     public class Senha
     {
-        private Dictionary<string, string> caracteresPossiveis;
-
-        public Senha(Dictionary<string, string> characterSets)
-        {
-            caracteresPossiveis = characterSets;
-        }
+        private int QuantidadeDeCaracteres { get; set; }
+        private string CaracteresPossiveis { get; set; }
         public  string GerarSenha(int maiusculas, 
                          int minusculas, 
                          int numeros, 
-                         int especiais, 
-                         Dictionary<string, string> caracteres)
+                         int especiais
+                         )
         {
-            var random = new Random();
+            Senha[] senhasPossiveis = new Senha[4]
+            {
+                new(){QuantidadeDeCaracteres = maiusculas, CaracteresPossiveis = "ABCDEFGHIJLMNOPQRSTUVXZ" },
+                new(){QuantidadeDeCaracteres = minusculas, CaracteresPossiveis = "abcdefghijlmnopqrstuvxz" },
+                new(){QuantidadeDeCaracteres = numeros, CaracteresPossiveis = "1234567890" },
+                new(){QuantidadeDeCaracteres = especiais, CaracteresPossiveis = "%$#@!" },
+            };
+
             var senha = new StringBuilder();
 
-            // Gere maiúsculas
-            AdicionarCaracteresParaSenha(senha, caracteres["Maiusculas"], maiusculas, random);
-
-            // Gere minúsculas
-            AdicionarCaracteresParaSenha(senha, caracteres["Minusculas"], minusculas, random);
-
-            // Gere números
-            AdicionarCaracteresParaSenha(senha, caracteres["Numeros"], numeros, random);
-
-            // Gere caracteres especiais
-            AdicionarCaracteresParaSenha(senha, caracteres["Especiais"], especiais, random);
-
-            // Embaralhe a senha
+            foreach (Senha s in senhasPossiveis)
+                {
+                    AdicionarCaracteresParaSenha(senha, s.CaracteresPossiveis, s.QuantidadeDeCaracteres);
+                }
             return Misturar(senha.ToString());
         }
 
-        private static void AdicionarCaracteresParaSenha(StringBuilder senha, string tipoDeCaracter, int count, Random random)
+        private static void AdicionarCaracteresParaSenha(StringBuilder senha, string tipoDeCaracter, int count)
         {
+            var random = new Random();
+
             for (int i = 0; i < count; i++)
             {
                 char randomCaracter = tipoDeCaracter[random.Next(0, tipoDeCaracter.Length)];
@@ -47,14 +43,12 @@ namespace GeradorDeSenhasCSharp.Models
         private static string Misturar(string str)
         {
             char[] caracteres = str.ToCharArray();
-            Random random = new Random();
+            Random random = new ();
 
             for (int i = caracteres.Length - 1; i > 0; i--)
             {
                 int j = random.Next(0, i + 1);
-                char temp = caracteres[i];
-                caracteres[i] = caracteres[j];
-                caracteres[j] = temp;
+                (caracteres[j], caracteres[i]) = (caracteres[i], caracteres[j]);
             }
 
             return new string(caracteres);
